ffda05e0b513e2bc467862158ba87e6a58f7e768
jiafeimao-gjf <1056615746@qq.com>
2024-06-06 10:20:58 +0800

    update

--- codeFormet.py
+++ codeFormet.py
@@ -9,38 +9,40 @@ code_path = "./problemset/" + suffix
 code_format_path = "./problemset-publish/" + suffix
 
 if __name__ == "__main__":
-    publish_file = input("please input filename:")
+    # publish_file = input("please input filename:")
     if os.path.exists(code_path):
         print('code path exist')
         for fileName in os.listdir(code_path):
             print(fileName)
-            if fileName in publish_file:
-                cf_out = open(code_format_path + fileName, mode="w", encoding="utf-8-sig")
-                
-                # ---
-                # title: 机器人的活动范围
-                # categories:
-                # - 算法题集
-                # tags:
-                # - 深度优先搜索
-                # date: 2019-12-26 23:12:31
-                # ---
-                cf_out.write("---\n")
-                cf_out.write("title: " + fileName.split(".")[0]+"\n")
-                cf_out.write("categories:\n")
-                cf_out.write("- leetcode\n")
-                cf_out.write("tags:\n")
-                cf_out.write("  - null\n")
-                now_time = datetime.datetime.now()
-                t_str = datetime.datetime.strftime(now_time,'%Y-%m-%d %H:%M:%S')
-                cf_out.write("date: " + t_str + "\n")
-                cf_out.write("---\n\n")
-                with open(code_path + fileName, mode="r") as cf:
+            # if fileName in publish_file:
+            cf_out = open(code_format_path + fileName, mode="w", encoding="utf-8-sig")
+            
+            # ---
+            # title: 机器人的活动范围
+            # categories:
+            # - 算法题集
+            # tags:
+            # - 深度优先搜索
+            # date: 2019-12-26 23:12:31
+            # ---
+            cf_out.write("---\n")
+            cf_out.write("title: " + fileName.split(".")[0]+"\n")
+            cf_out.write("categories:\n")
+            cf_out.write("- leetcode\n")
+            cf_out.write("tags:\n")
+            cf_out.write("  - null\n")
+            now_time = datetime.datetime.now()
+            t_str = datetime.datetime.strftime(now_time,'%Y-%m-%d %H:%M:%S')
+            cf_out.write("date: " + t_str + "\n")
+            cf_out.write("---\n\n")
+            with open(code_path + fileName, mode="r") as cf:
+                line = cf.readline()
+                while line:
+                    cf_out.write(line)
                     line = cf.readline()
-                    while line:
-                        cf_out.write(line)
-                        line = cf.readline()
 
-                cf_out.close()
+            cf_out.close()
+            # else:
+                # print(fileName,' not match ', publish_file)
     else:
         print('code path not exist')
